ad09b7d04038266173ef2f98912765b05a1fc0d6
Tom Dodd <tomdoddwork@hotmail.com>
2021-04-07 12:20:24 +0100

    Updated directory structure of simple service

---
+++ simple-service/src/main/java/com/simpleservice/simpleservice/controller/HelloController.java
@@ -0,0 +1,20 @@
+package com.simpleservice.simpleservice.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@RestController
+public class HelloController {
+
+    @RequestMapping("/")
+    public String index() {
+        return "Greetings from Spring Boot!";
+    }
+
+    @RequestMapping("/getCountry")
+    public String getCountry() {
+        return "Afghanistan";
+    }
+
+
+}
