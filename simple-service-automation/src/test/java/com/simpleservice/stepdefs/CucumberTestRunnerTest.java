3923534363711e70e9e499d763927741175b444c
pocketaces2 <58557417+pocketaces2@users.noreply.github.com>
2021-04-07 23:52:29 +0100

    Added json reports to cucumber

--- simple-service-automation/src/test/java/com/simpleservice/stepdefs/CucumberTestRunnerTest.java
+++ simple-service-automation/src/test/java/com/simpleservice/stepdefs/CucumberTestRunnerTest.java
@@ -7,7 +7,7 @@ import org.junit.runner.RunWith;
 
 @RunWith(Cucumber.class)
 @CucumberOptions(
-        plugin = {"pretty", "html:target/cucumber-reports"},
+        plugin = {"pretty", "html:target/cucumber-reports", "json:target/cucumber.json"},
         monochrome = true, 
         glue = "classpath:com/simpleservice/stepdefs",
         features = "classpath:com/simpleservice/features")
